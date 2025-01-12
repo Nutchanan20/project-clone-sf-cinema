205c410ab2977f87b774663c6491274fe5fc7393
ployns <pployns@gmail.com>
2025-01-12 17:17:28 +0700

    [FEAT] create dropdown-search component

---
+++ src/components/dropdown-search/index.tsx
@@ -0,0 +1,68 @@
+import { useRef, useState } from 'react';
+
+interface DropdownProps {
+  title: string;
+  list: string[];
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ title, list }) => {
+    const dropdownToggleRef = useRef<HTMLButtonElement | null>(null);
+  const dropdownMenuRef = useRef<HTMLUListElement | null>(null);
+
+  const handleClick = () => {
+    const dropdownToggle = dropdownToggleRef.current;
+    const dropdownMenu = dropdownMenuRef.current;
+
+    console.log(dropdownMenu); 
+    console.log(dropdownToggle); 
+
+      if (dropdownMenu?.className.includes('hidden')) {
+        dropdownMenu?.classList.add('block');
+        dropdownMenu?.classList.remove('hidden');
+        dropdownToggle?.classList.remove('text-black');
+        dropdownToggle?.classList.add('text-blue-800');
+      } else {
+        dropdownMenu?.classList.add('hidden');
+        dropdownMenu?.classList.remove('block');
+        dropdownToggle?.classList.add('text-black');
+        dropdownToggle?.classList.remove('text-blue-800');
+      }
+    
+  };
+
+  return (
+    
+      <div className="relative w-max mx-auto px-16 ">
+        <button
+          type="button"
+          ref={dropdownToggleRef}
+          className="px-5 py-2.5 rounded text-black text-sm tracking-wide border-none outline-none bg-white"
+          onClick={handleClick}
+        >
+          {title}
+        </button>
+
+        <ul
+          ref={dropdownMenuRef}
+          className="absolute hidden shadow-lg bg-white py-2 px-2 z-[1000] min-w-full w-max rounded max-h-96 overflow-auto"
+        >
+          <li className="mb-2">
+            <input
+              placeholder="Search here"
+              className="px-4 py-2.5 w-full rounded text-gray-800 text-sm border-none outline-blue-600 bg-blue-50 focus:bg-transparent"
+            />
+          </li>
+               {list.map((item, index) => (
+                <li key={index} className="py-2.5 px-4 hover:bg-blue-50 text-black text-sm cursor-pointer rounded">
+                    {item}
+                </li>
+          ))}
+
+        </ul>
+      </div>
+  );
+};
+
+
+
+export default Dropdown;
--- src/pages/home/components/search/index.tsx
+++ src/pages/home/components/search/index.tsx
@@ -1,4 +1,19 @@
+import {useRef} from 'react';
+import Dropdown from '../../../../components/dropdown-search';
+
 export default function Search() {
-    return <div className=" shadow-md h-32 mx-60 rounded-md ">
+
+  return (
+    <div className="flex items-center shadow-md rounded-md px-20 mx-80 h-28">
+    <Dropdown 
+    title='example 1'
+    list={['option 1', 'option 2']}
+  />
+  <Dropdown 
+    title='example 2'
+    list={['option 1', 'option 2']}
+  />
   </div>
+  )
+
 }
