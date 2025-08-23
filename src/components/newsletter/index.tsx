aac15872f40f291e8481a6d17b74648cf23f55c6
Nutchanan20 <nutchananbanmai96@gmail.com>
2025-08-23 18:11:07 +0700

    -edit newsletter input -insert an icon for newsletter -insert button for receiving news -adjusting the margin and padding of newsletter -adding a new photo in newsletter file

--- src/components/newsletter/index.tsx
+++ src/components/newsletter/index.tsx
@@ -1,9 +1,29 @@
+import Plane from "../newsletter/PaperPlane.svg"
+
 export default function Newsletter(){
 return(
-    <div className="fixed bottom-0 right-0 bg-red-600 w-[336px]">
-        <div>คลิกเพื่อรับข่าวสารจาก SF</div>
-        <div className="break-words">กรุณาระบุอีเมลที่ต้องการรับข่าวสารหรือโปรโมชั่น</div>
-        <input placeholder="อีเมล"></input>
+    <div className="fixed bottom-0 right-0 w-[336px] shadow-md">
+        <button className="bg-[#FFFFFF] flex justify-between items-center p-3 w-full">
+            <div className="flex-1 flex justify-center">
+                <img src={Plane} className="w-5 h-5 mr-2"></img>
+                <div className="text-[#2D64CF] font-bold">คลิกเพื่อรับข่าวสารจาก SF</div>
+            </div>
+            <div className="text-[#939494] text-lg">x</div>
+        </button>
+        
+        <div className="p-5 bg-[#F5F7FB]">
+            <div className="text-center mb-5">
+                กรุณาระบุอีเมลที่ต้องการรับข่าวสาร
+                <br />
+                หรือโปรโมชั่น
+            </div>
+            <div className="flex justify-center">
+                <input placeholder="อีเมล" className="rounded-sm border border-[#2D64CF] w-full h-10 px-2 focus:outline-none"></input>
+            </div>
+            <div className="flex justify-center mt-4">
+                <button className="text-white bg-gradient-to-r from-blue-700 to-sky-400 rounded-sm border w-full h-12 font-bold">รับข่าวสาร</button>
+            </div>
+        </div>
     </div>
 )
 
