27b3fea3097d6f27251e38d210484caba3b981d0
ployns <pployns@gmail.com>
2025-11-15 20:11:17 +0700

    [FEAT] Arrange elements for news

--- src/pages/home/components/news/index.tsx
+++ src/pages/home/components/news/index.tsx
@@ -8,13 +8,12 @@ export default function News() {
     {
       photo: news1,
       title:
-        "โปรพิเศษสุดคุ้ม แลกซื้อ Corn Stix ในราคาพิเศษ 30 บาท เมื่อซื้อ Combo Set ชุดใดก็ได้",
-      description: "",
+        "ผจญภัยในดินแดนออซ! พร้อมรับโปรหนังสุดคุ้ม “Wicked : For Good” ที่โรงภาพยนตร์ เอส เอฟ ",
+      description: "ภาพยนตร์เข้าฉายตั้งแต่วันที่ 20 พฤศจิกายน 2568 เป็นต้นไป",
     },
     {
       photo: news2,
-      title:
-        "โปรพิเศษสุดคุ้ม แลกซื้อ Corn Stix ในราคาพิเศษ 30 บาท เมื่อซื้อ Combo Set ชุดใดก็ได้",
+      title: "NEW MOVIES หนังใหม่สัปดาห์นี้ # 13 พฤศจิกายน 2568",
       description: "",
     },
   ];
@@ -25,16 +24,28 @@ export default function News() {
         <div className=" text-[#2d64cf] text-[26px] font-extrabold mt-[10px] mb-[15px]">
           ข่าวและกิจกรรม
         </div>
-        <div className="flex justify-center mx-[-20px] w-[1180px] h-[546.8px]">
-          <div className="grid grid-cols-2 px-[20px] mx-[-15px]">
-            {listNews.map((news, index) => (
-              <div className="w-[437.5px] h-[482px] px-[15px] bg-red-500">
-                <img src={news.photo} className="" />
-              </div>
-            ))}
+        <div className="flex mx-[-20px] w-[1180px] h-[546.8px]">
+          <div className="px-[20px] w-[885px] h-[522.25px]">
+            <div className="grid grid-cols-2 mx-[-15px] w-[875px] h-[522.25px]">
+              {listNews.map((news, index) => (
+                <div className="w-[437.5px] px-[15px] h-[482px]">
+                  <img src={news.photo} className="w-[407.5px] h-[305.625px]" />
+                  <div className="bg-white pl-[15px] pt-[20px] pr-[15px] pb-[40px] h-[169.781px]">
+                    <div className=" text-[#2d64cf] text-[16px] font-semibold text-center mb-[15px]">
+                      Movie News
+                    </div>
+                     <div className=" text-[##1e1f24] text-[14px] font-normal text-left mb-[15px]">
+                     {news.title}
+                    </div>
+                    <div className=" text-[#989898] text-[12px] font-normal text-center">
+                     {news.description}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="w-[255px] h-[547px] px-[20px] bg-blue-600"></div>
-
         </div>
       </div>
     </div>
